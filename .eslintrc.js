fc0cd42fb250af80dc5c3d22bdb263363d542884
tonny009 <tabassum.islam009@gmail.com>
2023-02-20 09:46:03 +0600

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'xo', 'prettier'],
+  overrides: [
+    {
+      extends: ['xo-typescript'],
+      files: ['*.ts', '*.tsx'],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['react'],
+  rules: {},
+}
